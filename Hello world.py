53cecd90b22c8ebf2e7e19b96ee01c0b894eca4c
Natbray <86750899+Natbray@users.noreply.github.com>
2023-05-10 15:18:05 +0930

    Update and Fix
    Fixed and updated several files

--- Hello world.py
+++ Hello world.py
@@ -1,3 +1,4 @@
 print ("hello from the otherside")
 print (" I am Cing")
-print (" Nice to meet you Natan and Adame")
+print (" Nice to meet you Nathan and Adam")
+# I Fixed the intentional spelling errors - NB
--- question.py
+++ question.py
@@ -1,1 +1,3 @@
-print (" Do you belive the fate ?")
+print (" Do you belive in fate ?")
+# Fixed and added flavour
+print("only as a thing to be broken")
